a9118cd6efe280858657003748333e24e3e310a1
YinMo19 <me@yinmo19.top>
2026-05-19 03:08:01 +0800

    Match Python stamina rounding

--- src/service/world.rs
+++ src/service/world.rs
@@ -359,8 +359,9 @@ impl StaminaImpl {
             .unwrap()
             .as_millis() as i64;
 
-        let calculated_stamina = Constants::MAX_STAMINA
-            - ((self.max_stamina_ts - current_time) / Constants::STAMINA_RECOVER_TICK) as i32;
+        let recovery_ticks = (self.max_stamina_ts - current_time) as f64
+            / Constants::STAMINA_RECOVER_TICK as f64;
+        let calculated_stamina = Constants::MAX_STAMINA - recovery_ticks.round() as i32;
 
         if calculated_stamina >= Constants::MAX_STAMINA {
             if self.stamina >= Constants::MAX_STAMINA {
